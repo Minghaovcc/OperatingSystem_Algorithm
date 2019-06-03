57f2d391299e18e33a35ad4caa9a1e401869434c
Qujihan <qujihan@163.com>
2019-06-03 16:31:37 +0800

    update

--- 进程调度算法/test.cpp
+++ 进程调度算法/test.cpp
@@ -9,10 +9,6 @@ void init();
 
 int main() {
 	init();
-	/*cout << "========先到先服务调度算法========" << endl;
-	PCB::FCFSrun();
-	cout << "========短作业优先调度算法========" << endl;
-	PCB::SJFrun();*/
 	cout << "=====动态高优先级优先调度算法=====" << endl;
 	PCB::PSArun();
 	return 0;
@@ -20,15 +16,9 @@ int main() {
 
 
 void init() {
-	//进程个数
-	//int time, priority, num;
-	//cout << "输入进程个数：" << endl;
-	//cin >> num;
+
 	PCB *pcb[3];
 	for (int i = 0; i < 3; i++) {
-		//cin >> time;
-		//进程优先级
-		//cin >> priority;
 		pcb[i] = new PCB(i+4, i+5);
 	}
 	
